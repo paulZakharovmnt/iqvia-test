f0e6ffb7c15bf8b27eecb2ab93c5ce8c9a346071
paulZakharovmnt <paulZakharov.mnt@gmail.com>
2020-12-18 12:29:16 -0500

    Add search city functions

--- src/Components/CitySearch/CitySearchPanel.js
+++ src/Components/CitySearch/CitySearchPanel.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import City from "./City";
+import "./CitySearchPanel.css";
 
-const CitySearchPanel = () => {
-  return <div></div>;
+const CitySearchPanel = ({ getCityWeatherClick }) => {
+  const [inputCitySearch, setInputCitySearch] = useState("");
+
+  const submitCitySearchInput = () => {
+    getCityWeatherClick(inputCitySearch);
+    setInputCitySearch("");
+  };
+  return (
+    <div className="city-search-panel">
+      <div className="city-search-input-container">
+        <input
+          value={inputCitySearch}
+          onChange={(event) => setInputCitySearch(event.target.value)}
+        />
+        <div onClick={submitCitySearchInput}> plus icon</div>
+      </div>
+      <div className="list-of-cities">
+        <City />
+        <div>City 2</div>
+        <div>City 3</div>
+      </div>
+      <div className="clear-btn-container">Clear BTN</div>
+    </div>
+  );
 };
 
 export default CitySearchPanel;

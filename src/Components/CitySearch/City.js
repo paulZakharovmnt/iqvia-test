419ac51f8280ef3c76eb9713f2d4ed4fb9d2258c
paulZakharovmnt <paulZakharov.mnt@gmail.com>
2020-12-18 12:28:59 -0500

    Add basic elements

--- src/Components/CitySearch/City.js
+++ src/Components/CitySearch/City.js
@@ -1,7 +1,21 @@
 import React from "react";
+import "./City.css";
 
 const City = () => {
-  return <div></div>;
+  return (
+    <div className="city">
+      <div className="city-data">
+        <h3>City Name</h3>
+        <p>- 12 C</p>
+        <p>Cloudy</p>
+        <p>Icon?</p>
+      </div>
+      <div className="city-control-icons">
+        <div>Update</div>
+        <div>Delete</div>
+      </div>
+    </div>
+  );
 };
 
 export default City;

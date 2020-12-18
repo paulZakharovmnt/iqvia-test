fd6f80cd83f6adfcaf968267e2f563e397da7580
paulZakharovmnt <paulZakharov.mnt@gmail.com>
2020-12-18 12:22:26 -0500

    Create basic components

---
+++ src/Components/CitySearch/CitySearchPanel.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CitySearchPanel = () => {
+  return <div></div>;
+};
+
+export default CitySearchPanel;
---
+++ src/Components/CityWeather/CityWeatherPanel.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CityWeatherPanel = () => {
+  return <div></div>;
+};
+
+export default CityWeatherPanel;

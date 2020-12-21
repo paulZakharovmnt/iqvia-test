e9d96a2a065535168a582d19b8ab2ae7ddef4927
paulZakharovmnt <paulZakharov.mnt@gmail.com>
2020-12-21 14:14:28 -0500

    Add city weather panel

--- src/Components/CityWeather/CityWeatherPanel.js
+++ src/Components/CityWeather/CityWeatherPanel.js
@@ -1,8 +1,50 @@
 import React from "react";
 import "./CityWeatherPanel.css";
+import OneDayForecast from "./OneDayForecast";
+import weatherIconMap from "../../core/weatherIconMap";
 
-const CityWeatherPanel = () => {
-  return <div className="city-weather-panel">CityWeatherPanel</div>;
+const CityWeatherPanel = ({
+  displayCityForecast,
+  handleShowDetailedCityForecast,
+}) => {
+  const currentCityWeather = displayCityForecast.current;
+  const weatherIcon = currentCityWeather.weather[0].icon;
+  const cityTemp = Math.floor(currentCityWeather.temp);
+
+  const fiveDaysforecast = displayCityForecast.daily.slice(1, 6);
+
+  return (
+    <div className="city-weather-box">
+      <div className="city-weather-panel-header">
+        <h1>{displayCityForecast.name}</h1>
+        <i
+          className="fas fa-sync-alt"
+          onClick={() => handleShowDetailedCityForecast(displayCityForecast)}
+        />
+      </div>
+      <div className="current-weather-container">
+        <div className="current-weather-icon">
+          <img src={weatherIconMap[weatherIcon]} alt="" />
+        </div>
+        <div className="current-weather-info">
+          <p>{cityTemp} C</p>
+          <p>{currentCityWeather.weather[0].main}</p>
+          <p>
+            Wind: {currentCityWeather.wind_speed}ms,{" "}
+            {currentCityWeather.wind_deg} deg
+          </p>
+          <p>{currentCityWeather.pressure}</p>
+        </div>
+      </div>
+      <div className="weather-forecast-container">
+        <div className="weather-forecast">
+          {fiveDaysforecast.map((dayForecast, id) => (
+            <OneDayForecast key={id} dayForecast={dayForecast} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CityWeatherPanel;

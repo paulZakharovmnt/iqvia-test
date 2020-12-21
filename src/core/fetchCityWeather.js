e18e8980e155f2757befd4e9d50eafb3ab35247c
paulZakharovmnt <paulZakharov.mnt@gmail.com>
2020-12-21 12:29:13 -0500

    Change API address request

--- src/core/fetchCityWeather.js
+++ src/core/fetchCityWeather.js
@@ -1,7 +1,9 @@
 import { weatherApi, apiKey } from "./weatherApi";
 
 const fetchCityWeather = async (inputCity) => {
-  return await fetch(`${weatherApi}?q=${inputCity}&appid=${apiKey}`)
+  return await fetch(
+    `${weatherApi}weather?q=${inputCity}&units=metric&appid=${apiKey}`
+  )
     .then((resp) => resp.json())
     .then((result) => {
       return result;

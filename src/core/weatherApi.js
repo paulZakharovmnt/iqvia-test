44580109b1cf9413ee3714f739591d9ead451527
paulZakharovmnt <paulZakharov.mnt@gmail.com>
2020-12-21 12:28:33 -0500

    Change API address

--- src/core/weatherApi.js
+++ src/core/weatherApi.js
@@ -1,2 +1,2 @@
 export const apiKey = "c51223c219d6aec8cb8c5210449bd859";
-export const weatherApi = "http://api.openweathermap.org/data/2.5/weather";
+export const weatherApi = "http://api.openweathermap.org/data/2.5/";

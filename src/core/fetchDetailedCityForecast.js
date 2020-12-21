578730f96439e3ed3e17c34df42bf547edb9aa9d
paulZakharovmnt <paulZakharov.mnt@gmail.com>
2020-12-21 12:28:57 -0500

    Split fetch forecast request into separate file

--- src/core/fetchDetailedCityForecast.js
+++ src/core/fetchDetailedCityForecast.js
@@ -1,11 +1,17 @@
 import { weatherApi, apiKey } from "./weatherApi";
 
-const fetchDetailedCityForecast = async (cityName) => {
-  fetch(
-    `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`
+const fetchDetailedCityForecast = async (cityInfo) => {
+  const apiOptions = "exclude=minutely,hourly,alerts&units=metric";
+  return await fetch(
+    `${weatherApi}onecall?lat=${cityInfo.coord.lat}&lon=${cityInfo.coord.lon}&${apiOptions}&appid=${apiKey}`
   )
     .then((resp) => resp.json())
-    .then((result) => console.log(result));
+    .then((result) => ({
+      current: result.current,
+      daily: result.daily,
+      coord: { lat: result.lat, lon: result.lon },
+      name: cityInfo.name,
+    }));
 };
 
 export default fetchDetailedCityForecast;

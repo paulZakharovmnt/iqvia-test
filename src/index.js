31008cb455e9e8194aa96df07f7d06904037d71d
paulZakharovmnt <paulZakharov.mnt@gmail.com>
2020-12-23 11:28:06 -0500

    Wrap everything in redux state

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import { Provider } from "react-redux";
+import allReducers from "./redux/reducers/allReducers";
+import { createStore } from "redux";
 import App from "./App";
 
+const store = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
